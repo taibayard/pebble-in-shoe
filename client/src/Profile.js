ad87540ae6672f77be50f95f38d4fd2d7b911763
adamredwoods <adamredwoods@users.noreply.github.com>
2018-01-24 19:10:25 -0800

    profile category totals

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
 
-let testData = [{food: 14}, {electronics: 50}, {food: 10}, {clothes: 12}, {electronics: 23}]
+//let testData = [{food: 14}, {electronics: 50}, {food: 10}, {clothes: 12}, {electronics: 23}]
+let testData = [ {cat: "food", total: "50"}, {cat: "electronics", total: "44"}, {cat: "food", total: "10"}, {cat: "clothes", total: "12"}, {cat: "electornics", total: "23"}];
+
 
 
 class Profile extends Component {
@@ -9,7 +11,8 @@ class Profile extends Component {
     super(props);
     this.state = {
       categoryData: {},
-      category: []
+      category: [],
+      catTotals: []
     }
   }
 
@@ -17,12 +20,40 @@ class Profile extends Component {
     fetch('/').then(function(response){
       return response.json();
     }).then(function(json){
-      this.setState({categoryData: json})
-    })
-      
 
+      let categoryList = this.state.category.slice();
+
+      this.setState({categoryData: json}, function() {
+         for(let i=0; i<this.state.categoryData.length; i++) {
+            let cat = categoryData[i].cat;
+            for( let j=0; j<categoryList.length; j++) {
+               if( cat!== categoryList[j]) {
+                  categoryList.push(cat);
+               }
+            }
+         }
+         this.setState({category: categoryList}, function() {
+            getCategoryTotals();
+         });
+      });
+    })
   }
 
+
+  getCategoryTotals = () => {
+     let catState = [];
+     for( let j=0; j<this.state.category.length; j++) {
+        let total=0;
+        for(let i=0; i<this.state.categoryData.length) {
+           if( this.state.category[j] === this.state.categoryData[j].cat) {
+             total += this.state.categoryData[j].total;
+          }
+        }
+        catState.push({ cat: this.state.category[j], total: total});
+     }
+
+   this.setState({catTotals: catState});
+ }
 
   render(){
     if(this.props.user && this.props.user.name){
@@ -32,9 +63,9 @@ class Profile extends Component {
           <h2>HELLO AGAIN {this.props.user.name}!</h2>
           <div className="resultsHeader">
             <div className="resultsTable">
-              <h2> Total # of Receipts: </h2>
+              <h2> Total # of Receipts: {this.state.categoryData.length}</h2>
               <h2> </h2>
-              <Categories category={this.state.category} />
+              <Categories category={this.state.catTotals} />
             </div>
           </div>
 
@@ -52,7 +83,7 @@ const Categories = (props) => {
   return (
     <div className="categoryTotals">
     {props.category.map(c => {
-      <h2> {c.name} : {c.total} </h2>
+      <h2> {c.cat} : {c.total} </h2>
     })}
     </div>
   )
