c9b7649f80975cbd747676c755cf0063a43f6207
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-01-24 20:31:50 -0800

    added post route on receipts.js

--- client/src/layout/Footer.js
+++ client/src/layout/Footer.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render(){
     return(
-        <div className="footer">
-            <span className="footer-text">Created by Brandi Butler &copy; {new Date().getFullYear()}</span>
+        <div>
         </div>
       );
   }
